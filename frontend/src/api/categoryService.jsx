3eadfc3b02fd50e8f8e091e01d572e4db0be35ad
emirdalgic <emirdalgic681@gmail.com>
2025-12-10 13:11:57 +0300

    added save, rename, and delete methods to categoryService

--- frontend/src/api/categoryService.jsx
+++ frontend/src/api/categoryService.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import axiosInstance from './axiosConfig'
 
-export const getAllCategories=async()=>{
+export const getAllCategories = async () => {
     const res = await axiosInstance.get("/categories/list")
     return res.data
 }
 
-export const getCategoryById=async(id)=>{
+export const getCategoryById = async (id) => {
     const res = await axiosInstance.get(`/categories/list/${id}`)
+    return res.data
+}
+
+
+export const createCategory = async (categoryName) => {
+    const res = await axiosInstance.post("/categories/save", null, {
+        params: { name: categoryName }
+    })
+    return res.data
+}
+
+export const renameCategoryById = async (id, categoryName) => {
+    const res = await axiosInstance.put(`/categories/update/${id}`, null, {
+        params: { name: categoryName }
+    })
+    return res.data
+}
+
+export const deleteCategoryById = async (id) => {
+    const res = await axiosInstance.delete(`/categories/delete/${id}`)
     return res.data
 }

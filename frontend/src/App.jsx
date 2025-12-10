38ddc9988b5776300b66ff48dd3baf0230b0f230
emirdalgic <emirdalgic681@gmail.com>
2025-12-10 13:12:26 +0300

    implemented footer and configure conditional admin routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
@@ -13,32 +13,46 @@ import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
 import OrdersPage from './pages/OrdersPage'
 import AddressPage from './pages/AddressPage'
+import AdminPage from './pages/AdminPage'
+import AdminProductForm from './pages/AdminProductForm'
 
 function App() {
+
+  const location = useLocation()
+
+  const isAdminPages = location.pathname.startsWith("/admin")
+
   return (
-    <div>
-      <Navbar />
+    <div className="flex flex-col min-h-screen">
 
-      <Routes>
-        {/*public routes*/}
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/products' element={<ProductListPage />} />
-        <Route path='/products/category/:categoryId' element={<ProductListPage />} />
-        <Route path='/products/:productId' element={<ProductDetailsPage />} />
-        {/*private routes*/}
-        <Route path='/cart' element={<CartPage />} />
-        <Route path='/profiles/:profileId' element={<ProfilePage />} />
-        <Route path='/checkout' element={<CheckoutPage />} />
-        <Route path='/orders/history' element={<  OrdersPage />}></Route>
-        <Route path='/profile/my-addresses' element={<AddressPage />}></Route>
+      {!isAdminPages && <Navbar />}
 
-        {/*not found*/}
-        <Route path='*' element={<NotFoundPage />} />
+      <div className="flex-grow">
+        <Routes>
+          {/*public routes*/}
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/products' element={<ProductListPage />} />
+          <Route path='/products/category/:categoryId' element={<ProductListPage />} />
+          <Route path='/products/:productId' element={<ProductDetailsPage />} />
+          <Route path='/cart' element={<CartPage />} />
+          {/*private routes*/}
+          <Route path='/profiles/:profileId' element={<ProfilePage />} />
+          <Route path='/checkout' element={<CheckoutPage />} />
+          <Route path='/orders/history' element={<  OrdersPage />}></Route>
+          <Route path='/profile/my-addresses' element={<AddressPage />}></Route>
+          {/* --- Admin Routes --- */}
+          <Route path='/admin' element={<AdminPage />}></Route>
+          <Route path='/admin/product/new' element={<AdminProductForm />}></Route>
+          <Route path='/admin/product/edit/:productId' element={<AdminProductForm />}></Route>
 
-      </Routes>
-      <Footer />
+          {/*not found*/}
+          <Route path='*' element={<NotFoundPage />} />
+
+        </Routes>
+      </div>
+      {!isAdminPages && <Footer />}
     </div>
   )
 }
